39fcec93fc8312f784c6bbc9b2f9d862657f8c85
root <rahul@kali>
2021-07-18 19:43:30 -0400

    fix

--- AllSafe_Technologies_Pvt_Ltd_solving/settings.py
+++ AllSafe_Technologies_Pvt_Ltd_solving/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-re1p6w&dy#cl0^-02y97550)^rk!27$p&gz%@%b)e3hqmvw3d+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
